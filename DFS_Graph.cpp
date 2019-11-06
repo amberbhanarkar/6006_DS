721334a5c0221cf82bd1fb849dd40889b22b3280
Amber Bhanarkar <amberbhanarkar@gmail.com>
2019-11-06 22:07:30 +0530

    DFS in Graph

---
+++ DFS_Graph.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class Graph{
+	int V;
+	list<int> *adj;
+	void DFSUtil(int v, bool visited[]);
+	public:
+		Graph(int V);
+		void addEdge(int v, int w);
+		void DFS(int v);
+};
+
+Graph::Graph(int v)
+{
+	this->V=v;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w);
+}
+
+void Graph::DFSUtil(int v, bool visited[])
+{
+	visited[v]=true;
+	list<int>::iterator i;
+	for(i=adj[v].begin();i!=adj[v].end(); ++i)
+	{
+		if(!visited[*i])
+		{
+			DFSUtil(*i,visited);
+		}
+	}
+}
+
+void Graph::DFS(int v)
+{
+	bool *visited = new bool[V];
+	for(int i=0;i<V;i++)
+	{
+		visited[i]=false;
+	}
+	for(int i=0;i<V;i++)
+	{
+		if(visited[i]==false)
+		{
+			DFSUtil(i,visited);
+		}
+	}
+}
+
+main()
+{
+	Graph g(4);
+	g.addEdge(0,1);
+	g.addEdge(0,2);
+	g.addEdge(1,2);
+	g.addEdge(1,3);
+	g.addEdge(2,3);
+	cout<<"DFS is:";
+	g.DFS(2);
+}
